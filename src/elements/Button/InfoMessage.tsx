b550b8ef84203693821aa17276744159cb1cf157
Bakhtiyar Zubairov <irookanji@gmail.com>
2022-08-16 10:31:39 +0200

    Add information message snackbar(MUI) element

---
+++ src/elements/Button/InfoMessage.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Stack from '@mui/material/Stack'
+import Snackbar from '@mui/material/Snackbar'
+import MuiAlert, { AlertProps } from '@mui/material/Alert'
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />
+})
+
+const InfoMessage = () => {
+  const [openSnackBar, setOpenSnackBar] = React.useState(false)
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setOpenSnackBar(false)
+  }
+
+  return (
+    <div>
+      <Stack spacing={2} sx={{ width: '100%' }}>
+        <Snackbar open={openSnackBar} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity='error' sx={{ width: '100%' }}>
+            This is a error message!
+          </Alert>
+        </Snackbar>
+      </Stack>
+    </div>
+  )
+}
+
+export default InfoMessage
